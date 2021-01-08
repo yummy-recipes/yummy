f9070bdebf950b9a49d61c048fef01bb180eba1f
ertrzyiks <mateusz.derks@gmail.com>
2021-01-08 21:17:57 +0100

    Clean up bootstrap

--- packages/cms/config/functions/bootstrap.js
+++ packages/cms/config/functions/bootstrap.js
@@ -1,7 +1,4 @@
 'use strict';
-const fs = require('fs')
-const YAML = require('yaml')
-const FormData = require('form-data')
 
 /**
  * An asynchronous bootstrap function that runs before
@@ -24,94 +21,8 @@ async function findOrCreateTag(name) {
   return newTag
 }
 
-async function importRecipe(slug, category, content, coverPath, gallery) {
-  console.log(` - recipe ${slug}`)
-
-  const [_, configString, ...rest] = content.split('---')
-
-  const markdown = rest.join('---')
-
-  const config = YAML.parse(configString)
-
-  const [headline, ingredients, directions] = markdown.split('<!---- splitter ---->')
-
-  const tags = await Promise.all(config.tags.map(name => findOrCreateTag(name)))
-
-  const recipe = await strapi.services.recipe.create({
-    title: config.title,
-    slug,
-    headline: headline.trim(),
-    directions: directions.trim(),
-    category: category.id,
-    published_at: config.date,
-    tags: tags.map(tag => tag.id),
-    preparationTime: parseInt(config.required_time, 10),
-    ingredients: ingredients.trim(),
-    seo: {
-      htmlTitle: config.title,
-      htmlDescription: headline.trim()
-    }
-  })
-
-  const file = {
-    size: 10,
-    path: coverPath,
-    name: 'cover.jpg',
-    type: 'image/jpeg',
-    lastModifiedDate: null,
-    hash: null
-  }
-
-  await strapi.plugins.upload.services.upload.uploadToEntity({
-    id: recipe.id,
-    model: 'recipe',
-    field: 'cover'
-  }, file)
-
-  if (gallery && gallery.length > 0) {
-    await strapi.plugins.upload.services.upload.uploadToEntity({
-      id: recipe.id,
-      model: 'recipe',
-      field: 'gallery'
-    }, gallery.map(galleryFile => ({
-      size: 10,
-      path: galleryFile,
-      name: 'gallery.jpg',
-      type: 'image/jpeg',
-      lastModifiedDate: null,
-      hash: null
-    })))
-  }
-}
-
-function getGallery(category, slug) {
-  return new Promise(resolve => {
-    fs.readdir('../../../yummy-content/recipes/' + category + '/' + slug + '/gallery', async (err, files) => {
-      const gallery = files || []
-      resolve(gallery.map(file => '../../../yummy-content/recipes/' + category + '/' + slug + '/gallery/' + file))
-    })
-  })
-}
-
-async function importByCategory(slug) {
-  const category = await strapi.services.category.findOne({ slug })
-
-  console.log(`IMPORTING ${slug}`)
-  return new Promise(resolve => {
-    fs.readdir('../../../yummy-content/recipes/' + slug, async (err, files) => {
-      for (const file of files) {
-        const content = fs.readFileSync('../../../yummy-content/recipes/' + slug + '/' + file + '/index.md')
-        const gallery = await getGallery(slug, file)
-        await importRecipe(file, category, content.toString(), '../../../yummy-content/recipes/' + slug + '/' + file + '/cover.jpg', gallery)
-      }
-
-      resolve()
-    })
-  })
-}
-
-async function enableRead() {
-  const role = await strapi.query('role', 'users-permissions').findOne({ type: 'public' })
+async function enableRead(type) {
+  const role = await strapi.query('role', 'users-permissions').findOne({ type })
   for (const permission of role.permissions) {
     if (permission.type === 'application' && permission.action === 'find') {
       await strapi.query('permission', 'users-permissions').update({ id: permission.id }, { ...permission, enabled: true } )
@@ -239,11 +150,8 @@ async function setupTestFixtures() {
 }
 
 module.exports = async () => {
-  // strapi.plugins.upload.services.upload
-  // console.log(Object.keys(strapi.plugins.upload.services.upload))
-
-
-  await enableRead()
+  const roleToRead = process.env.NODE_ENV === 'test' ? 'public' : 'authenticated'
+  await enableRead(roleToRead)
   await updateUploadSettings()
 
   const numberOfCategories = await strapi.services.category.count()
@@ -261,15 +169,8 @@ module.exports = async () => {
   const numberOfRecipes = await strapi.services.recipe.count()
 
   if (numberOfRecipes === 0) {
-
     if (process.env.NODE_ENV === 'test') {
       await setupTestFixtures()
-    } else if (process.env.SEED_CONTENT === 'true') {
-      fs.readdir('../../../yummy-content/recipes', async (err, files) => {
-        for (const file of files) {
-          await importByCategory(file)
-        }
-      })
     }
   }
 
