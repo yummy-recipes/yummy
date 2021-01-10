947b39d9f9fcf0b2a0b21fe36a76a3dc7f6bfe93
ertrzyiks <mateusz.derks@gmail.com>
2021-01-10 08:23:27 +0100

    Add default auth header

--- packages/cms/config/server.js
+++ packages/cms/config/server.js
@@ -6,4 +6,9 @@ module.exports = ({ env }) => ({
       secret: env('ADMIN_JWT_SECRET', '9dc18e1a9ded255ebbd144f5bb02762c'),
     },
   },
+  webhooks: {
+    defaultHeaders: {
+      'x-token': env('DEPLOY_HOOK_TOKEN', 'sudo'),
+    },
+  }
 });
