549e20b0821a18bf3c1589f3e1e4b354f6e041eb
ertrzyiks <mateusz.derks@gmail.com>
2021-04-03 16:02:14 +0200

    Remove public folder

--- apps/blog/public/component---src-templates-error-404-js-d26e67918400f1716425.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{ID13:function(n,e,a){"use strict";a.r(e),a.d(e,"default",(function(){return c}));var t=a("ERkP"),i=a.n(t),o=a("W5jw"),u=a.n(o);function c(){return i.a.createElement("div",{className:u.a.layout},"Było i nie ma :(")}}}]);
-//# sourceMappingURL=component---src-templates-error-404-js-d26e67918400f1716425.js.map
