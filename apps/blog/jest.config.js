a62e35865bfa118d8063e953b496fad9bdd8be89
ertrzyiks <mateusz.derks@gmail.com>
2021-12-28 15:48:11 +0100

    Fix jest

--- apps/blog/jest.config.js
+++ apps/blog/jest.config.js
@@ -1,5 +1,23 @@
-const config = require('@yummy/core/jest.config.js')
-
+const CI = process.env.CI
 module.exports = {
-  ...config
+  testEnvironment: 'jsdom',
+  transform: {
+    '^.+\\.jsx?$': '<rootDir>/../../config/jest/jest-preprocess.js',
+    '^.+\\.svg$': '<rootDir>/../../config/jest/svgrTransform.js'
+  },
+  moduleNameMapper: {
+    '.+\\.(css|styl|less|sass|scss)$': 'identity-obj-proxy',
+    '.+\\.(jpg|jpeg|png|gif|eot|otf|webp|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/../../config/jest/mocks/file-mock.js',
+  },
+  testPathIgnorePatterns: ['node_modules', '.cache', 'cypress', 'examples'],
+  transformIgnorePatterns: ['node_modules/(?!(gatsby)/)'],
+  globals: {
+    __PATH_PREFIX__: '',
+  },
+  testURL: 'http://localhost',
+  setupFiles: [
+    '<rootDir>/../../config/jest/loadershim.js',
+    './jest/mocks/gatsby.js'
+  ],
+  reporters: ['default'].concat(CI ? [['jest-junit', {outputDirectory: 'test-results/jest'}]] : []),
 }
