6603b3e6e5b772dd94614293d18f99f2636c3a2e
Mateusz Derks <mateusz.derks@gmail.com>
2022-02-01 08:47:56 +0100

    Migrate visual tests (#36)
    Use chromatic instead custom solution

--- apps/blog/src/stories/header.stories.js
+++ apps/blog/src/stories/header.stories.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
 
-import Header from '../components/header'
+// import Header from '../components/header'
 
 const categories = [
   {name: 'Category1', slug: 'category1'},
@@ -9,12 +9,12 @@ const categories = [
 ]
 
 storiesOf('Header', module)
-  .add('full header', () => (
-    <Header categories={categories} fullVersion={true} />
-  ))
-  .add('header on subsection', () => (
-    <Header categories={categories} fullVersion={false} subsection='obiady' />
-  ))
-  .add('header on a recipe page', () => (
-    <Header categories={categories} fullVersion={false} isSingleRecipe={true} />
-  ))
+  // .add('full header', () => (
+  //   <Header categories={categories} fullVersion={true} />
+  // ))
+  // .add('header on subsection', () => (
+  //   <Header categories={categories} fullVersion={false} subsection='obiady' />
+  // ))
+  // .add('header on a recipe page', () => (
+  //   <Header categories={categories} fullVersion={false} isSingleRecipe={true} />
+  // ))
