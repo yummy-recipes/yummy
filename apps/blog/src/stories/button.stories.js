4251fb97de5354a6f9223f33454980f27d00c740
ertrzyiks <mateusz.derks@gmail.com>
2022-02-01 21:46:43 +0100

    Fix other stories

--- apps/blog/src/stories/button.stories.js
+++ apps/blog/src/stories/button.stories.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import { storiesOf } from '@storybook/react'
 
 import * as transparent from '../components/button/button.transparent.module.sass'
 import * as transparentNoBorder from '../components/button/button.transparent-noborder.module.sass'
 
-storiesOf('Button', module)
-  .add('transparent', () => (
-    <button className={transparent.button}>Action</button>
-  ))
-  .add('transparent-noborder', () => (
-    <button className={transparentNoBorder.button}>Action</button>
-  ))
+export default {
+  title: 'Button'
+}
+
+export const Transparent = () => <button className={transparent.button}>Action</button>
+
+export const TransparentNoBorder = () => <button className={transparentNoBorder.button}>Action</button>
