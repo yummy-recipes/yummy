c1b60fb5eb0c3b6edc51486669460e93ff28ba0d
ertrzyiks <mateusz.derks@gmail.com>
2022-04-09 14:47:12 +0200

    Fix storybook

--- apps/blog/.storybook/main.js
+++ apps/blog/.storybook/main.js
@@ -1,37 +1,41 @@
-
+const { mergeConfig } = require('vite')
 
 module.exports = {
   stories: ['../src/stories/*.jsx'],
   core: {
-    builder: process.env.USE_VITE_BUILDER ? 'storybook-builder-vite' : 'webpack5',
+    builder: process.env.USE_VITE_BUILDER
+      ? 'storybook-builder-vite'
+      : 'webpack5',
   },
-  addons: [
-    '@storybook/addon-actions',
-    '@storybook/addon-links',
-  ],
-  webpackFinal: async config => {
+  addons: ['@storybook/addon-actions', '@storybook/addon-links'],
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {})
+  },
+  webpackFinal: async (config) => {
     // Transpile Gatsby module because Gatsby includes un-transpiled ES6 code.
     config.module.rules[0].exclude = [/node_modules\/(?!(gatsby)\/)/]
     // Use babel-plugin-remove-graphql-queries to remove static queries from components when rendering in storybook
     config.module.rules[0].use[0].options.plugins.push(
-      require.resolve("babel-plugin-remove-graphql-queries")
+      require.resolve('babel-plugin-remove-graphql-queries'),
     )
 
     // Replace default css loader
     config.module.rules[3].use = [
       require.resolve('style-loader'), // creates style nodes from JS strings
-      { // css modules support
+      {
+        // css modules support
         loader: require.resolve('css-loader'),
         options: {
           esModule: true,
           importLoaders: 1,
           modules: {
-            namedExport: true
-          }
+            namedExport: true,
+          },
         },
-      }
+      },
     ]
 
     return config
-  }
+  },
 }
--- apps/blog/src/stories/homepage.stories.jsx
+++ apps/blog/src/stories/homepage.stories.jsx
@@ -1,25 +1,27 @@
 import React from 'react'
 
 import PostListPage from '../templates/post-list'
+import { createBlogListQueryQueryMock } from '../templates/post-list.generated'
 
-
-const BackgroundDecorator = storyFn => <div style={{backgroundColor: '#e98500'}}>{storyFn()}</div>
+const BackgroundDecorator = (storyFn) => (
+  <div style={{ backgroundColor: '#e98500' }}>{storyFn()}</div>
+)
 
 export default {
-  title: 'PostListPage',
+  title: 'Pages/PostListPage',
   component: PostListPage,
-  decorators: [
-    BackgroundDecorator
-  ]
+  decorators: [BackgroundDecorator],
 }
 
 export const Primary = () => (
-<PostListPage
-  data={{}}
-  pageContext={{
-    currentPage: 1,
-    totalPages: 10
-  }}
-  location={null}
-/>
+  <PostListPage
+    data={createBlogListQueryQueryMock()}
+    pageContext={{
+      currentPage: 1,
+      totalPages: 10,
+    }}
+    location={{
+      pathname: '/',
+    }}
+  />
 )
