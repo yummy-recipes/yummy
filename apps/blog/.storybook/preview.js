99a9ec5935d0fe133dd96dc9821c216d0e3fb285
ertrzyiks <mateusz.derks@gmail.com>
2022-06-13 22:19:12 +0200

    Update storybook

--- apps/blog/.storybook/preview.js
+++ apps/blog/.storybook/preview.js
@@ -1,5 +1,3 @@
-import { action } from '@storybook/addon-actions'
-
 // Gatsby's Link overrides:
 // Gatsby Link calls the `enqueue` & `hovering` methods on the global variable ___loader.
 // This global object isn't set in storybook context, requiring you to override it to empty functions (no-op),
@@ -9,14 +7,14 @@ window.___loader = {
   hovering: () => {},
 }
 // This global variable prevents the "__BASE_PATH__ is not defined" error inside Storybook.
-window.__BASE_PATH__ = "/"
+window.__BASE_PATH__ = '/'
 
 // Navigating through a gatsby app using gatsby-link or any other gatsby component will use the `___navigate` method.
 // In Storybook, it makes more sense to log an action than doing an actual navigate. Check out the actions addon docs for more info: https://storybook.js.org/docs/react/essentials/actions
-window.___navigate = pathname => {
-  action("NavigateTo:")(pathname)
+window.___navigate = (pathname) => {
+  console.log('NavigateTo:', pathname)
 }
 
 export const parameters = {
-  chromatic: { viewports: [360, 1200] }
+  chromatic: { viewports: [360, 1200] },
 }
--- apps/blog/tailwind.config.js
+++ apps/blog/tailwind.config.js
@@ -1,8 +1,53 @@
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      primary: {
+        DEFAULT: '#E69900',
+        50: '#FFECC7',
+        100: '#FFE5B3',
+        200: '#FFD88A',
+        300: '#FFCA61',
+        400: '#FFBD38',
+        500: '#FFAF0F',
+        600: '#E69900',
+        700: '#AD7400',
+        800: '#754E00',
+        900: '#3D2900',
+      },
+    },
+    spacing: {
+      1: '8px',
+      2: '12px',
+      3: '16px',
+      4: '24px',
+      5: '32px',
+      6: '48px',
+    },
+    fontSize: {
+      xs: '.875rem',
+      base: '1rem',
+      lg: '1.125rem',
+      xl: '1.25rem',
+      '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+      '5xl': '3rem',
+      '6xl': '4rem',
+    },
+    lineHeight: {
+      none: '1rem',
+      normal: '1.5rem',
+    },
+    borderWidth: {
+      DEFAULT: '1px',
+    },
   },
   plugins: [],
 }
