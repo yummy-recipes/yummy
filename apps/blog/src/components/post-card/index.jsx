f5d0a8a98db0506e1b44dbff7c906bf43282425f
Mateusz Derks <mateusz.derks@gmail.com>
2022-08-30 22:33:54 +0200

    Migrate post card to tailwind (#70)

--- apps/blog/src/components/post-card/index.jsx
+++ apps/blog/src/components/post-card/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
-import * as styles from './post-card.module.css'
-
 export default function PostCard({ href, className, children }) {
   return (
-    <Link to={href} className={[styles.card, className].join(' ')}>
+    <Link
+      to={href}
+      className={[
+        'bg-white text-black flex flex-col overflow-hidden no-underline',
+        className,
+      ].join(' ')}
+    >
       {children}
     </Link>
   )
