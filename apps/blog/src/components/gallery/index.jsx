b737a2a8a06719704396bcd7277e31427cc1e4c5
ertrzyiks <mateusz.derks@gmail.com>
2022-09-23 21:34:03 +0200

    Migrate gallery to tailwind

--- apps/blog/src/components/gallery/index.jsx
+++ apps/blog/src/components/gallery/index.jsx
@@ -1,17 +1,21 @@
 import React, { Component } from 'react'
 import Img from 'gatsby-image'
-import * as styles from './gallery.module.css'
 import Loadable from 'react-loadable'
-import 'react-image-lightbox/style.css';
-import { getPrevImgUrl, getNextImgUrl, getPrevPhotoIndex, getNextPhotoIndex } from '../../utils/galleryBrowserHelper'
+import 'react-image-lightbox/style.css'
+import {
+  getPrevImgUrl,
+  getNextImgUrl,
+  getPrevPhotoIndex,
+  getNextPhotoIndex,
+} from '../../utils/galleryBrowserHelper'
 
 const LoadableLightbox = Loadable({
   loader: () => import('react-image-lightbox'),
   loading() {
     return null
   },
-  delay: 200
-});
+  delay: 200,
+})
 
 export default class Gallery extends Component {
   constructor(props) {
@@ -19,7 +23,7 @@ export default class Gallery extends Component {
 
     this.state = {
       isLightboxOpen: false,
-      photoIndex: 0
+      photoIndex: 0,
     }
   }
 
@@ -33,44 +37,51 @@ export default class Gallery extends Component {
 
     const GalleryImages = images.map((img, ndx) => {
       return (
-        <button className={styles.imageWrapper} type="button" onClick={() => {
-          this.setState({
-            isLightboxOpen: true,
-            photoIndex: ndx
-          })
-        }} key={ndx}>
+        <button
+          className="p-0 border-none mr-2 mb-2"
+          type="button"
+          onClick={() => {
+            this.setState({
+              isLightboxOpen: true,
+              photoIndex: ndx,
+            })
+          }}
+          key={ndx}
+        >
           <Img
             fluid={img.childImageSharp.small}
             alt={'Picture of the dish'}
-            className={styles.galleryImage}
+            className="w-[135px] h-[135px] md:w-[200px] md:h-[200px]"
             key={ndx}
           />
         </button>
       )
     })
 
-    const imgSources = images.map(img => img.childImageSharp.large.src)
+    const imgSources = images.map((img) => img.childImageSharp.large.src)
 
     return (
-      <div className={styles.galleryContainer}>
-        <h2 className={styles.galleryHeader}>Galeria</h2>
-        <div className={styles.gallery}>
-          {GalleryImages}
-        </div>
-        {isLightboxOpen &&
+      <div className="mt-5 md:pb-3">
+        <h2 className="font-medium">Galeria</h2>
+        <div className="flex flex-wrap">{GalleryImages}</div>
+        {isLightboxOpen && (
           <LoadableLightbox
             mainSrc={imgSources[photoIndex]}
             prevSrc={getPrevImgUrl(photoIndex, imgSources)}
             nextSrc={getNextImgUrl(photoIndex, imgSources)}
             onCloseRequest={() => this.setState({ isLightboxOpen: false })}
-            onMovePrevRequest={() => this.setState({
-              photoIndex: getPrevPhotoIndex(photoIndex, imgSources.length)
-            })}
-            onMoveNextRequest={() => this.setState({
-              photoIndex: getNextPhotoIndex(photoIndex, imgSources.length)
-            })}
+            onMovePrevRequest={() =>
+              this.setState({
+                photoIndex: getPrevPhotoIndex(photoIndex, imgSources.length),
+              })
+            }
+            onMoveNextRequest={() =>
+              this.setState({
+                photoIndex: getNextPhotoIndex(photoIndex, imgSources.length),
+              })
+            }
           />
-        }
+        )}
       </div>
     )
   }
