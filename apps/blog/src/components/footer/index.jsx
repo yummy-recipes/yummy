b68d5babbebe8dd9663b02ce26e6f573594599de
ertrzyiks <mateusz.derks@gmail.com>
2022-09-23 21:19:06 +0200

    Remove footer className prop

--- apps/blog/src/components/footer/index.jsx
+++ apps/blog/src/components/footer/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react'
 
-export default function Footer({ className }) {
+export default function Footer() {
   return (
-    <footer
-      className={['py-6 mt-6 md:mt-8 font-semibold border-t', className].join(
-        ' ',
-      )}
-    >
+    <footer className="py-6 mt-6 md:mt-8 font-semibold border-t">
       <div className="px-4">© 2018-{new Date().getFullYear()} J. M. Derks</div>
     </footer>
   )
