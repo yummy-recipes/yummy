5c3c03939b55442a992a31e2bdde34215d42a57c
ertrzyiks <mateusz.derks@gmail.com>
2022-10-21 21:31:38 +0200

    Migrate pagination to tailwind

--- apps/blog/src/components/paginator/index.jsx
+++ apps/blog/src/components/paginator/index.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import { Link } from 'gatsby'
 import { calculatePages } from '../../utils/paginationHelper'
 import { paginationElementTypes } from '../../utils/paginationConsts'
-import * as styles from './paginator.module.css'
 import Button from '../button'
 import ChevronLeft from '../icons/ChevronLeft'
 import ChevronRight from '../icons/ChevronRight'
@@ -35,10 +34,12 @@ function PaginationLinkElement({
   subsectionPath,
   elementIndex,
 }) {
-  const listItemStyles =
-    elementType === paginationElementTypes.CURRENT_PAGE
-      ? [styles.pageNavItem, styles.currentPage].join(' ')
-      : styles.pageNavItem
+  const listItemStyles = [
+    'inline py-1 list-none',
+    ...(elementType === paginationElementTypes.CURRENT_PAGE
+      ? ['font-medium']
+      : []),
+  ].join(' ')
 
   const testId =
     elementType === paginationElementTypes.CURRENT_PAGE
@@ -50,7 +51,7 @@ function PaginationLinkElement({
     case paginationElementTypes.PAGE:
       elementContent = (
         <Link
-          className={styles.pageLink}
+          className="text-blue-500 no-underline"
           to={generatePagePath(subsectionPath, pageNumber)}
         >
           {pageNumber}
@@ -102,22 +103,22 @@ export default function Paginator({ currentPage, totalPages, currentPath }) {
   }
 
   return (
-    <nav className={styles.paginator}>
+    <nav className="text-center mb-2 flex">
       {currentPage > 1 && (
-        <Button as={Link} to={prevPagePath} className={styles.prev}>
-          <ChevronLeft className={styles.icon} />
-          <span className={styles.link_text}>Poprzednia</span>
+        <Button as={Link} to={prevPagePath} className="mb-2">
+          <ChevronLeft className="h-3 align-baseline mx-1" />
+          <span className="hidden md:block">Poprzednia</span>
         </Button>
       )}
+
+      <ol className="text-xs px-3 py-2 w-full m-0">{pages}</ol>
 
       {currentPage < totalPages && (
-        <Button as={Link} to={nextPagePath} className={styles.next}>
-          <span className={styles.link_text}>Następna</span>
-          <ChevronRight className={styles.icon} />
+        <Button as={Link} to={nextPagePath} className="mb-2">
+          <span className="hidden md:block">Następna</span>
+          <ChevronRight className="h-3 align-baseline mx-1" />
         </Button>
       )}
-
-      <ol className={styles.pageList}>{pages}</ol>
     </nav>
   )
 }
