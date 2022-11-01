cbcb6e6b7f2e9f9c674ea96e1a4aaec16b8a1bc1
Mateusz Derks <mateusz.derks@gmail.com>
2022-11-01 14:06:46 +0100

    Migrat two more components to tailwind (#167)

---
+++ apps/blog/src/stories/time_to_prepare.stories.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import TimeToPrepare from '../components/time_to_prepare'
+
+export default {
+  title: 'TimeToPrepare',
+  component: TimeToPrepare,
+}
+
+export const Default = () => <TimeToPrepare>10</TimeToPrepare>
