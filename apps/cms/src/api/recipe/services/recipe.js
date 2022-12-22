441d2e15d322f68894f9e79b51dd48b5f87ad750
ertrzyiks <mateusz.derks@gmail.com>
2022-12-22 22:11:02 +0100

    Fix bootstrap

--- apps/cms/src/api/recipe/services/recipe.js
+++ apps/cms/src/api/recipe/services/recipe.js
@@ -1,10 +1,10 @@
 'use strict'
 
+const { createCoreService } = require('@strapi/strapi').factories
+
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/services.html#core-services)
  * to customize this service
  */
 
-module.exports = ({ strapi }) => {
-  return {}
-}
+module.exports = createCoreService('api::recipe.recipe')
